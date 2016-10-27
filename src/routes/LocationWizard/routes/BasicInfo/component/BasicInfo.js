1ad35c71b6061b20a04a7f6047178b7f6be8fdae
Rajanikanth.Katakam <rajanikanth.katakam@ggktech.com>
2016-10-27 19:54:27 +0530

    BasicInfo UI changes

--- src/routes/LocationWizard/routes/BasicInfo/component/BasicInfo.js
+++ src/routes/LocationWizard/routes/BasicInfo/component/BasicInfo.js
@@ -1,54 +1,71 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import validate from '../validations/basicInfoValidation'
-import {Panel, ControlLabel} from 'react-bootstrap/lib'
+import {Panel, ControlLabel, Checkbox} from 'react-bootstrap/lib'
 import 'styles/basicInfoStyles.scss'
-
-
 
 export const BasicInfo = (props) => {
     const {locationTypes} = props
 return (
-            <div>
-                <Panel header={<ControlLabel>Basic Information</ControlLabel>}>
-                        <form onSubmit={props.basicInfoSubmit} onChange={props.onchange} >
-                            <div className="row form-group" style={{marginBottom:'0px'}}>
-                                    <div className="col-sm-6 ">
-                                         <div className="col-sm-4 basic-form-input">
-                                                <label htmlFor="locationNamelocationName">locationName</label>
-                                             <Field name="locationName" component="input" type="text"/>
-                                        </div>
+  <div className="row">
+    <div className="col-xs-12">
+        <div className="box">
+            <div className="box-header">
+                <h3 className="box-title">Basic Information</h3>
+                <div className="box-tools pull-right">
+                    &nbsp;
+                </div>
+            </div>
+            <div className="box-body">
+                <div className="margin-bottom-sm padding-top">
+                    <div className="row">
+                        <form onSubmit={props.basicInfoSubmit} onChange={props.onchange}>
+                            <div className="col-sm-12">
+                                <div className="row">
+                                    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <label className="control-label" for="locationName"> Name </label>
+                                        <Field name="locationName" component="input" className="form-control" type="text" />
                                     </div>
-                                    <div className="col-sm-6">
-                                        
-                                        <Field name="allowOutages" id="allowOutages" component="input" type="checkbox" label="AllowOutages"/>
-                                    </div>
-                            </div>
-                            <div className="row form-group" style={{marginBottom:'0px'}}>
-                                <div className="col-sm-6">
-                                    <div className="col-sm-4 basic-form-input">
-                                            <label htmlFor="parentLocation">parentLocation </label>
-                                             <Field name="parentLocation" component="input" type="text"/>
-                                    </div>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="col-sm-4 basic-form-input">
-                                        <Field name="type" component="select">
+                                    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <Checkbox name="allowOutages">
+                                            Allow Outages at Location
+                                        </Checkbox>
+                                    </div>     
+                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <label className="control-label" for="type"> Parent Location </label>
+                                        <Field name="type" component="select" className="form-control">
                                             <option></option>
                                             <option value="ff0000">Red</option>
                                             <option value="00ff00">Green</option>
                                             <option value="0000ff">Blue</option>
                                         </Field>
                                     </div>
+                                    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <label className="control-label" for="locationName"> Name </label>
+                                        <Field name="locationName" component="input" className="form-control" type="text" />
+                                    </div>
+                                    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <label className="control-label" for="locationName"> Name </label>
+                                        <Field name="locationName" component="input" className="form-control" type="text" />
+                                    </div>
+                                    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <label className="control-label" for="locationName"> Name </label>
+                                        <Field name="locationName" component="input" className="form-control" type="text" />
+                                    </div>
+                                    <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                                        <label className="control-label" for="locationName"> Name </label>
+                                        <Field name="locationName" component="input" className="form-control" type="text" />
+                                    </div>
                                 </div>
                             </div>
 
-                            <button type="submit" className="btn btn-success">Submit</button>
                         </form>
-                  
-                </Panel>
+                    </div>
+                </div>
             </div>
-
+        </div>
+    </div>
+</div>
         )
     
 }
