e11630c080527c84b046ab18ca746d5249eb0bbe
Rajanikanth.Katakam <rajanikanth.katakam@ggktech.com>
2016-10-28 19:27:50 +0530

    Main Header Changes

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -4,10 +4,10 @@ import Header from '../../components/Header'
 export const CoreLayout = ({ children }) => (
   <div>
     <Header />
-    <div  style={{minHeight: "949px",paddingTop:'80px'}}>
+    <div  style={{paddingTop:'80px'}}>
 		<div id="content_frame" className="content-frame" style={{width:"100%",position:'relative',display:"block"}}>
 			{children}
-      	</div>
+    </div>
     </div>
   </div>
 )
--- src/routes/LocationWizard/components/LocationWizard.js
+++ src/routes/LocationWizard/components/LocationWizard.js
@@ -8,23 +8,42 @@ import WizardTabs from "./Wizardtabs"
 
 export const LocationWizard =(props)=> {
 return (
-    <div className="row" style={{paddingTop:'50px'}}>
-                        <div className="col-md-2">
-                            LeftMenu Space.
-                            {props.data}
-                        </div>
-                        <div className="col-md-10">
-                         <div className="board">
-                        <WizardTabs/>
-                         <div className="tab-content">
-                            
-                                {props.children}                          
-                            </div>                        
-                        </div>
-                        </div>
-                    </div>
+<div>
+    <div className="row">
+        <div className="col-md-2 col-xs-12">
+            LeftMenu Space.
+            {props.data}
+        </div>
+        <div className="col-md-10 col-xs-12">
+            <section id="content_header" className="content-header">
+                <h1>Location Wizard<small>OMS Location wizard</small></h1>
+                <ol className="breadcrumb">
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-home"></i> Dashboard (Home)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            OMS
+                        </a>
+                    </li>
 
-            
+                    <li className="active"><strong>Location Wizard</strong></li>
+                </ol>
+            </section>
+            <section className="content">
+                <div className="board">
+                    <WizardTabs />
+                    <div className="tab-content">
+                        {props.children}
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+  
         )
     
 }
