fb57ac76b4dfcf932e7d798de8dc2c2abf13a6df
Rajanikanth.Katakam <rajanikanth.katakam@ggktech.com>
2016-11-09 18:08:16 +0530

    Wizard tabs highlight changes

--- src/routes/LocationWizard/components/WizardTabs.js
+++ src/routes/LocationWizard/components/WizardTabs.js
@@ -21,69 +21,69 @@ return (
             <ul className="nav nav-tabs">
                 <div className="liner"></div>
                 <li>
-                    <Link activeClass="active" className="wizard-item" offset={-280} to="basicInfo" spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" offset={-232} to="basicInfo" spy={true} smooth={true} duration={500}>
                     <span className="round-tabs one">
                         <i className="glyphicon glyphicon-home"></i>
                     </span>
-                    <span className="tab-title"><small>Basic Information</small></span></Link>
+                    <span className="tab-title hidden-xs"><small>Basic Information</small></span></Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" offset={-280} to="credential" spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" offset={-232} to="credential" spy={true} smooth={true} duration={500}>
                     <span className="round-tabs two">
                         <i className="glyphicon glyphicon-gift"></i>
                     </span>
-                    <span className="tab-title"><small>Credential Management</small></span></Link>
+                    <span className="tab-title hidden-xs"><small>Credential Management</small></span></Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" offset={-280}to ="unitcharacteristics" spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" offset={-232}to ="unitcharacteristics" spy={true} smooth={true} duration={500}>
                     <span className="round-tabs three">
                         <i className="glyphicon glyphicon-user"></i>
                     </span>
-                    <span className="tab-title"><small>Unit Characteristics</small></span></Link>
+                    <span className="tab-title hidden-xs"><small>Unit Characteristics</small></span></Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" offset={-280} to="systemintegration" spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" offset={-232} to="systemintegration" spy={true} smooth={true} duration={500}>
                     <span className="round-tabs four">
                         <i className="glyphicon glyphicon-comment"></i>
                     </span>
-                    <span className="tab-title"><small>System Integration</small></span></Link>
+                    <span className="tab-title hidden-xs"><small>System Integration</small></span></Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" to="workflows" offset={-280} spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" to="workflows" offset={-232} spy={true} smooth={true} duration={500}>
                     <span className="round-tabs five">
                         <i className="glyphicon glyphicon-folder-open"></i>
                     </span>
-                    <span className="tab-title"><small>&nbsp;&nbsp;WorkFlows</small></span></Link>
+                    <span className="tab-title hidden-xs"><small>&nbsp;&nbsp;WorkFlows</small></span></Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" to="users" offset={-280} spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" to="users" offset={-232} spy={true} smooth={true} duration={500}>
                     <span className="round-tabs six">
                         <i className="glyphicon glyphicon-user"></i>
                     </span>
-                    <span className="tab-title"><small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Users</small></span></Link>
+                    <span className="tab-title hidden-xs"><small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Users</small></span></Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" to="equipment" offset={-280} spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" to="equipment" offset={-232} spy={true} smooth={true} duration={500}>
                     <span className="round-tabs seven">
                         <i className="glyphicon glyphicon-indent-right"></i>
                     </span>
-                    <span className="tab-title"><small>&nbsp;&nbsp;&nbsp;Equipment</small></span>
+                    <span className="tab-title hidden-xs"><small>&nbsp;&nbsp;&nbsp;Equipment</small></span>
                     </Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" to="gateways" offset={-280} spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" to="gateways" offset={-232} spy={true} smooth={true} duration={500}>
                     <span className="round-tabs two">
                         <i className="glyphicon glyphicon-pencil"></i>
                     </span>
-                    <span className="tab-title"><small>&nbsp;&nbsp;&nbsp;Gateways</small></span>
+                    <span className="tab-title hidden-xs"><small>&nbsp;&nbsp;&nbsp;Gateways</small></span>
                     </Link>
                 </li>
                 <li>
-                    <Link activeClass="active" className="wizard-item" offset={-280} to="datahistorian" spy={true} smooth={true} duration={500}>
+                    <Link activeClass="active" className="wizard-item" offset={-232} to="datahistorian" spy={true} smooth={true} duration={500}>
                     <span className="round-tabs three">
                         <i className="glyphicon glyphicon-pencil"></i>
                     </span>
-                    <span className="tab-title"><small>Data Historian</small></span>
+                    <span className="tab-title hidden-xs"><small>Data Historian</small></span>
                     </Link>
                 </li>
 
