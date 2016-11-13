2971e8b6b798951d385cbf75ca1ccfcd3b52df56
Rajanikanth.Katakam <rajanikanth.katakam@ggktech.com>
2016-11-13 17:31:13 +0530

    Added Scroll bar to left menu

--- src/routes/LocationWizard/components/LocationLeftMenu.js
+++ src/routes/LocationWizard/components/LocationLeftMenu.js
@@ -3,30 +3,33 @@ import 'styles/locationStyles.scss'
 import {Panel, Nav, NavItem} from 'react-bootstrap'
 import {Link}from 'react-router'
 import TreeView from './BootstrapTreeView';
-
+import { Scrollbars } from 'react-custom-scrollbars';
 
 export const LocationLeftMenu =(props)=> {
-    
+     var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      x = w.innerWidth || e.clientWidth || g.clientWidth;
+var leftMenuHeight = w.innerHeight||e.clientHeight||g.clientHeight;
   return(   
-     <aside className="main-sidebar control-sidebar-dark">
-        <ul className="nav nav-tabs nav-justified control-sidebar-tabs">
-            <li id='location-header' className="active">
-             <h5>Locations </h5>                
-                <Link to="/location" data-toggle="tab">
-                   <i className="fa fa-plus-circle fa-2x"></i> 
-                </Link>
-            </li>
-        </ul>
-
+<aside className="main-sidebar control-sidebar-dark">
+    <ul className="nav nav-tabs nav-justified control-sidebar-tabs">
+        <li id='location-header' className="active">
+            <h5>Locations </h5>
+            <Link to="/location" data-toggle="tab">
+                <i className="fa fa-plus-circle fa-2x"></i>
+            </Link>
+        </li>
+    </ul>
+    <Scrollbars style={{ height: leftMenuHeight }}>
         <div className="tab-content">
             <div className="tab-pane active" id="control-sidebar-menutree-tab">
-               
-                   <TreeView data={props.Locations} leftMenuDropdownClickEvent={props.leftMenuDropdownClickEvent}/>
-                
+                <TreeView data={props.Locations} leftMenuDropdownClickEvent={props.leftMenuDropdownClickEvent} />
             </div>
         </div>
-
-    </aside>
+    </Scrollbars>
+</aside>
     );
 }
 
