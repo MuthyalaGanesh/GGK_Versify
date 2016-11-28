46b38e055a2be012a9ceb4adc71df4de36a9eccc
AravindGona <aravind.gona@ggktech.com>
2016-11-28 13:08:58 +0530

    unitcharacteristics bug fixes

--- src/components/DatePicker/DatePickerField.js
+++ src/components/DatePicker/DatePickerField.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { DateField } from 'react-date-picker'
 import 'react-date-picker/index.css'
-export const DatePickerField = (props) => (
+export const DatePickerField = (props) =>{
+console.log(props.defaultValue)
+ return(
     <div>
-         <DateField {...props.input}  value={props.input.value} dateFormat="MM/DD/YYYY" defaultDate={props.defaultValue?new Date(props.defaultValue):null} />
-    </div>
+    <DateField onChange={props.input.onChange}
+    			value ={props.input.value || props.defaultValue }
+         	
+        	dateFormat="MM/DD/YYYY"           
+        	footer={false}          
+        	updateOnDateClick={true}          
+        	collapseOnDateClick={true}          
+        	 />
+ </div>        
 )
-
+}
 export default DatePickerField
